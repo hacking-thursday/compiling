7a7bd3245ef3de9839359102c377d9d2a30432f3
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-15 22:38:28 +0800

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    req = f.read()
+
 setup(
     name='compiling',
     version='0.0.0',
     description='Compiling message generator',
     long_description=open('README.md').read(),
+    install_requires=req
     scripts=['bin/compiling'],
     url='https://github.com/hacking-thursday/compiling',
 )
