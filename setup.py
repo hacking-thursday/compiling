ba5276f95355eabc64d5c5b6831845c858822015
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-15 22:23:13 +0800

    Add bin/compiling and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='compiling',
+    version='0.0.0',
+    description='Compiling message generator',
+    long_description=open('README.md').read(),
+    scripts=['bin/compiling'],
+    url='https://github.com/hacking-thursday/compiling',
+)
