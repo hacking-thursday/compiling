8e61f63a93758ac4a4efd2c1dd9b8d6ca492b4eb
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-16 13:21:38 +0800

    Add source_generator

---
+++ compiling/_utils.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+import yaml
+
+_data = {}
+
+def _load_data():
+    dataroot = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
+    for filename in os.listdir(dataroot):
+        if not filename.endswith(".yml"):
+            continue
+        src = filename[:-4]
+
+        with open(os.path.join(dataroot, filename)) as f:
+            _data[src] = list(yaml.safe_load_all(f))
+
+def source_generator(src):
+    if src not in _data:
+        raise NotImplementedError("source_generator does not support src = {}".format(src))
+
+    random.seed()
+
+    while True:
+        yield _data[src][random.randint(0, len(_data[src])-1)]
+
+_load_data()
