b28c3c5ff218e5abda43b7eb49ed9ce56534e9c5
sthsieh <sthsieh@synology.com>
2014-05-16 11:16:07 +0800

    add basic packaging structure
    Signed-off-by: sthsieh <sthsieh@synology.com>

--- setup.py
+++ setup.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python
-from distutils.core import setup
-
+from setuptools import setup, find_packages
+import codes
 import os
+import re
+import compiling
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
-    req = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codes.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with codes.open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirement = f.read()
 
 setup(
     name='compiling',
-    version='0.0.0',
+    version=compiling.__version__,
     description='Compiling message generator',
-    long_description=open('README.md').read(),
-    install_requires=req
-    scripts=['bin/compiling'],
+    long_description=long_description,
     url='https://github.com/hacking-thursday/compiling',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='compile',
+    install_requires=requirement,
+    package_data={
+    },
+    data_files=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'bin/compiling',
+        ],
+    },
 )
